75798b42daf4355c2f0de17ab16dbf691752c80a
PTL <jhiou01@hotmail.com.tw>
2020-01-19 12:02:13 +0800

    0119morning, saveJPEG need work

---
+++ PTLJava/src/tw/org/iii/apps/tool/MyDrawer.java
@@ -0,0 +1,128 @@
+package tw.org.iii.apps.tool;
+// 搭配MySig用的下方簽名畫面
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class MyDrawer extends JPanel{	//繼承JPanel, MyD可稱為自訂視窗元件	
+	private LinkedList<LinkedList<HashMap<String, Integer>>> lines, recycle; //用LL存放XY座標資料構成線, 用HM表示, 一條線不夠, 再包一層做很多條線
+	
+	public MyDrawer() {
+		setBackground(Color.YELLOW);
+		
+		lines = new LinkedList<>();
+		recycle = new LinkedList<>();
+		
+		MyMouseListener listener = new MyMouseListener();
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {	//視窗裡, 元件的樣貌是由此段設定的
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(3));
+		
+		for(LinkedList<HashMap<String, Integer>> line : lines ) {	//巡訪每條線, 後來多線時用這塊包住
+			
+			for(int i = 1; i<line.size(); i++) {	//用線畫出點
+				HashMap<String, Integer> p0 = line.get(i-1);
+				HashMap<String, Integer> p1 = line.get(i);
+				g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));//輸入key取得value
+			}
+		
+		}
+		
+	}
+	
+	public void clear() {
+		lines.clear();
+		repaint();
+	}
+	public void undo() throws Exception{
+		if(lines.size()>0) {
+			recycle.add(lines.removeLast());		//LL原本的功能, 丟掉的給recycle
+			repaint();
+		}else {
+			throw new Exception();	//保留彈性讓使用者決定
+		}
+	}
+	public void redo() {
+		if(recycle.size()>0) {						
+			lines.add(recycle.removeLast());		//拿回來給lines
+			repaint();			
+		}
+	}
+	public void saveJPEG(File file) {
+		BufferedImage img = new BufferedImage(	//創造空白畫布 
+				getWidth(), getHeight(), 
+				BufferedImage.TYPE_INT_RGB);
+		
+		Graphics g = img.getGraphics();		
+		print(g);								//把現在的畫面畫到空白畫布上
+		try {
+			ImageIO.write(img, "jpg", file);	//也可以不要存檔, 只把畫面io出去
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+		
+	}
+	
+	private class MyMouseListener extends MouseAdapter{	//內部類別
+		@Override
+		public void mousePressed(MouseEvent e) {	//按著; click是按下放開款
+			super.mousePressed(e);
+//			System.out.println("press");
+			recycle.clear();
+			LinkedList<HashMap<String, Integer>> line = new LinkedList<>();	//做完lines後再新增這段
+			lines.add(line);
+			
+			HashMap<String, Integer> point = new HashMap<>();
+			point.put("x", e.getX());
+			point.put("y", e.getY());
+			line.add(point);
+		}
+		
+		@Override
+			public void mouseDragged(MouseEvent e) {
+				super.mouseDragged(e);
+//				System.out.println("drag");
+				HashMap<String, Integer> point = new HashMap<>();
+				point.put("x", e.getX());
+				point.put("y", e.getY());
+				
+				lines.getLast().add(point);	//原本是下面單行把值給一條線, 多條以後用getLast抓到最後一條線, 給值
+//				line.add(point);	//把區域變數point的值給line以後point就沒用, 但值已經交給line
+				repaint();			//重新繪製整個畫面的概念
+		}
+	}
+		
+//		@Override
+//			public void mouseReleased(MouseEvent e) {	//最後沒用到
+//				super.mouseReleased(e);
+//				System.out.println("release");
+//			}
+
+	private class Point {		//此可用來取代HashMap
+		private int x, y;
+		void setXY(int x, int y) {
+			this.x = x; this.y=y;
+		}
+		int getX() {return x;}
+		int getY() {return y;}
+	}
+}
--- PTLJava/src/tw/ptl/apps/MySignature.java
+++ PTLJava/src/tw/ptl/apps/MySignature.java
@@ -1,13 +1,65 @@
 package tw.ptl.apps;
-
+// 搭配tool pack裡的MyDrawer
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tw.org.iii.apps.tool.MyDrawer;
 
 public class MySignature extends JFrame{
+	private MyDrawer myDrawer;
+	private JButton clear, undo, redo, saveJPEG;
+	
 	public MySignature() {
 		super("Signature");
 		setLayout(new BorderLayout());
+		
+		myDrawer = new MyDrawer();
+		add(myDrawer, BorderLayout.CENTER);
+		
+		JPanel topLine = new JPanel(new FlowLayout());
+		clear = new JButton("Clear");
+		clear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.clear();
+			}
+		});
+		undo = new JButton("Undo");
+		undo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					myDrawer.undo();
+				}catch(Exception e2) {
+					System.out.println(e2);
+				}
+			}
+		});
+		redo = new JButton("Redo");
+		redo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.redo();
+			}
+		});
+		saveJPEG = new JButton("Save JPEG");
+		saveJPEG.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.saveJPEG(new File("dir2/ptl.jpg"));
+			}
+		});
+		
+		topLine.add(clear); topLine.add(undo); topLine.add(redo);
+		topLine.add(saveJPEG);
+		add(topLine, BorderLayout.NORTH);
 	
 		setVisible(true);
 		setSize(800, 480);
