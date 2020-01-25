dee2ff7de74f668bbcf4c07a211a9018989bbf20
Ian <zeltkimo+github@gmail.com>
2020-01-25 13:38:01 +0800

    test how long

--- PTLJava/src/tw/ptl/apps/Ztest.java
+++ PTLJava/src/tw/ptl/apps/Ztest.java
@@ -6,7 +6,7 @@ public class Ztest {
 
 	public static void main(String[] args) {
 
-
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		
 		
 	}
